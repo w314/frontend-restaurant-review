486a4cd12029c5386ef923e4e44660c578e17eca
Piroska Werner <piroska.werner@gmail.com>
2018-10-02 12:05:44 -0500

    feat: Marker delete problem solved

--- js/main.js
+++ js/main.js
@@ -119,7 +119,8 @@ resetRestaurants = (restaurants) => {
 
   // Remove all map markers
   if (self.markers) {
-    self.markers.forEach(marker => marker.remove());
+    // self.markers.forEach(marker => marker.remove());
+    self.markers.forEach(marker => marker.setMap(null));
   }
   self.markers = [];
   self.restaurants = restaurants;
