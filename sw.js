b391a259b0b8062040cc327cb599878607f52a4e
Piroska Werner <piroska.werner@gmail.com>
2018-10-10 16:01:56 -0500

    feat: Correcting lint errors in sw.js and deleting comments

--- sw.js
+++ sw.js
@@ -1,12 +1,13 @@
-//set cache name
-let staticCacheName = 'restaurantReviewCache5';
+'use strict';
+// set cache name
+let staticCacheName = 'restaurantReviewCache';
 
-//cache resources at service worker installation
-this.addEventListener('install', (event) => {
+// cache resources at service worker installation
+this.addEventListener('install', () => {
 	let urlsToCache = [
 		'/',
 		'index.html',
-		// 'restaurant.html',
+		'restaurant.html',
 		'css/styles.css',
 		'css/frontPage.css',
 		'css/restaurantDetails.css',
@@ -36,90 +37,36 @@ this.addEventListener('install', (event) => {
 
 
 // delete any earlier cache at service worker activation
-this.addEventListener('activate', (event) => {
+this.addEventListener('activate', () => {
 	new Promise.all (
 		caches.keys()
 		.then((cacheNames) => {
 			cacheNames
-			.filter((cacheName) => {return cacheName.startsWith('rest') && cacheName != staticCacheName;})
-			.map((cacheName) => {return caches.delete(cacheName);})
+			.filter((cacheName) => {
+				return cacheName.startsWith('rest') && cacheName != staticCacheName;
+			})
+			.map((cacheName) => {return caches.delete(cacheName);});
 		})
-	)
+	);
 });
 
 
-//interrupt fetch request and serve from cache if resource is cached
-//add url to cache if request was not cached before 
+// interrupt fetch request and serve from cache if resource is cached
+// add url to cache if request was not cached before
 this.addEventListener('fetch', (event) => {
 	event.respondWith(
 		caches.match(event.request)
 		.then((response) => {
-			if(response) {
+			if (response) {
 				return response;
 			}
-			// return fetch(event.request).then((response) => {addUrlToCache(response)});
 			return fetch(event.request).then(addUrlToCache(event.request));
-			// return fetch(event.request);
 		})
-	)
+	);
 });
 
-//add url to cache
+// add url to cache
 function addUrlToCache(request) {
 	caches.open(staticCacheName)
 	.then((cache) => {cache.add(request.url);});
-	// console.log(request.url);
 }
-
-
-/*RESPONDING FOR EVIL IN CASE OF 404 */
-// this.addEventListener('fetch', (event) => {
-// 	event.respondWith (
-// 		fetch(event.request)
-// 		.then((response) => {
-// 			if(response.status === 404) {
-// 				return fetch('/img/dr-evil.gif')
-// 			}
-// 			return response;
-// 		})
-// 		.catch(() => {return new Response('total failure')})
-// 	);
-// });
-
-/* RESPONDING WIHT TEXT */
-// this.addEventListener('fetch', (event) => { 
-// 	event.respondWith(
-// 		////responding with text
-// 		new Response('szia <b>mizujs</b>', {
-// 			headers : {
-// 				'Content-Type' : 'text/html'
-// 			}
-// 		})
-// 	);
-// });
-
-
-/* REPLACING PICTURES WITH EVIL PICTURE */
-// this.addEventListener('fetch', (event) => {
-// 	console.log(event);
-// 	if(event.request.url.endsWith('jpg')) {
-// 		event.respondWith(fetch('/img/dr-evil.gif'));
-// 	}
-// });
-// 	
-
-/* RESPONDING WITH CUSTOM MESSAGES */
-// this.addEventListener('fetch', (event) => {
-// 	event.respondWith(
-// 		fetch(event.request)
-// 		.then((response) => {
-// 			if(response.status === 404) {
-// 				return new Response('Not found');
-// 			}
-// 			return response;
-// 		})
-// 		.catch(() => {return new Response('total failure')})
-// 	);
-// });
-
-
